12d1031068ab113b64365ec3596b489be64cb2da
Marian Zinkevych <maryan.20083@gmail.com>
2024-04-21 13:24:00 +0300

    added Spring Bean Annotation

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -4,26 +4,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext classPathXmlApplicationContext =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        ClassicalMusic musicBean = classPathXmlApplicationContext.getBean("musicBean", ClassicalMusic.class);
-        System.out.println(musicBean.getSong());
-        ClassicalMusic musicBean2 = classPathXmlApplicationContext.getBean("musicBean", ClassicalMusic.class);
-        System.out.println(musicBean2.getSong());
-
-
-//        MusicPlayer musicPlayer = classPathXmlApplicationContext.getBean("musicPlayer", MusicPlayer.class);
-//        musicPlayer.playSong();
-//        System.out.println(musicPlayer.getName() + musicPlayer.getVolume());
-//        MusicPlayer secondMusicPlayer = classPathXmlApplicationContext.getBean("musicPlayer", MusicPlayer.class);
-//        boolean compraison = musicPlayer == secondMusicPlayer;
-//
-//        System.out.println(musicPlayer.getVolume());
-//
-//        secondMusicPlayer.setVolume(35);
-//        System.out.println(secondMusicPlayer.getVolume());
-//        System.out.println(musicPlayer.getVolume());
-
+        Music music = classPathXmlApplicationContext.getBean("rockMusic", Music.class);
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+        musicPlayer.playSong();
         classPathXmlApplicationContext.close();
     }
 }
