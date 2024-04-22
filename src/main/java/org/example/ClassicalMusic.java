62d8a86d7c1b8d55b4b1706531d63a6fb33f3fd8
Marian Zinkevych <maryan.20083@gmail.com>
2024-04-22 20:09:32 +0300

    added PostConstruct and PreDestroy need <dependency>     <groupId>javax.annotation</groupId>     <artifactId>javax.annotation-api</artifactId>     <version>1.3.2</version> </dependency>

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -3,10 +3,11 @@ package org.example;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.List;
 @Component
-@Scope("prototype")
 public class ClassicalMusic implements Music{
 
     private final List<String> songs = new ArrayList<>();
@@ -17,6 +18,14 @@ public class ClassicalMusic implements Music{
         songs.add("Night on Bald Mountain");
     }
 
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
     @Override
     public List<String> getSong() {
         return songs;
