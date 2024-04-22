b0c1165a463988f6eb101dd5bc3a27c3af5d7fe2
Marian Zinkevych <maryan.20083@gmail.com>
2024-04-22 20:25:11 +0300

    added JavaSpringConfiguration

---
+++ src/main/java/org/example/SpringConfiguration.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("org.example")
+@PropertySource("classpath:musicPlayer.properties")
+public class SpringConfiguration {
+
+}
